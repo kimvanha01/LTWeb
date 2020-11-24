4b06d64df14e324d6ab7b14c6f3e4fbe60012ce2
kimvanha01 <kimvanha1999@gmail.com>
2020-11-24 21:49:38 +0700

    Home

---
+++ JS/signup.js
@@ -0,0 +1,90 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const lastname = document.getElementById('lastname');
+const firstname = document.getElementById('firstname');
+const dateofbirth = document.getElementById('dateofbirth');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	const lastnameValue = lastname.value.trim();
+	const firstnameValue = firstname.value.trim();
+	const dateofbirthValue = dateofbirth.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	if(firstnameValue === '') {
+		setErrorFor(firstname, 'First Name cannot be blank');
+	} else {
+		setSuccessFor(firstname);
+	}
+	if(lastnameValue === '') {
+		setErrorFor(lastname, 'Last Name cannot be blank');
+	} else {
+		setSuccessFor(lastname);
+	}
+	
+	if(dateofbirthValue == ''){
+		setErrorFor(dateofbirth,'Date of birth cannot be blank');
+	}else{
+		setSuccessFor(dateofbirth);
+	}
+
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	}else if (!isPassword(passwordValue)) {
+		setErrorFor(password, 'Not a valid password');
+	}else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+function isPassword(password){
+	return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
+}
